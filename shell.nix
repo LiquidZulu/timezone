d1363d34f9e93ce0c57f8cdc52771ab0eb351a30
LiquidZulu <liquidzulu@pm.me>
2024-04-27 17:49:19 +0100

    Nix tooling

---
+++ shell.nix
@@ -0,0 +1,23 @@
+{ pkgs ? import <nixpkgs> { } }:
+let
+  rust_overlay = import (builtins.fetchTarball
+    "https://github.com/oxalica/rust-overlay/archive/master.tar.gz");
+  pkgs = import <nixpkgs> { overlays = [ rust_overlay ]; };
+  #rustVersion = "latest";
+  rustVersion = "1.74.0";
+  rust = pkgs.rust-bin.stable.${rustVersion}.default.override {
+    extensions = [
+      "rust-src" # for rust-analyzer
+      "rust-analyzer"
+    ];
+  };
+in pkgs.mkShell {
+  buildInputs = [ rust ] ++ (with pkgs;
+    [
+      pkg-config
+      # other dependencies
+      #gtk3
+      #wrapGAppsHook
+    ]);
+  RUST_BACKTRACE = 1;
+}
